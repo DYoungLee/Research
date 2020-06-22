6d3b958b1821076ce2cbbf87464cc08d8a520ae7
Dae-Young Lee <38064728+DaeyoungLee0@users.noreply.github.com>
2020-06-22 22:54:16 +0900

    Update Example.m

--- 2. Pathological states recognition/Example.m
+++ 2. Pathological states recognition/Example.m
@@ -48,42 +48,50 @@ B = 512;
 % Calculation of Multiscale Entropy methods
 % CG: coarse-graining / MA: moving-averaging
 w = 100;
-for i=1:50
-    MSE_chf(i,:) = MSE_CG(RR_chf(1+i*w:(i+1)*w), m_se, r, tau, scale, 1);
-    MS_young(i,:) = MSE_CG(RR_young(1+i*w:(i+1)*w), m_se, r, tau, scale, 1);
-    MSE_elderly(i,:) = MSE_CG(RR_elderly(1+i*w:(i+1)*w), m_se, r, tau, scale, 1);
-
-    MDE_chf(i,:) = MDE_MA(RR_chf(1+i*w:(i+1)*w), m_de, B, scale);
-    MDE_young(i,:) = MDE_MA(RR_young(1+i*w:(i+1)*w), m_de, B, scale);
-    MDE_elderly(i,:) = MDE_MA(RR_elderly(1+i*w:(i+1)*w), m_de, B, scale);
+for i=1:floor(length(RR_chf)/w)
+    MSE_chf(i,:) = MSE_CG(RR_chf(1+(i-1)*w:i*w), m_se, r, tau, scale, 1);
+    MDE_chf(i,:) = MDE_MA(RR_chf(1+(i-1)*w:i*w), m_de, B, scale);
+end
+for i=1:floor(length(RR_young)/w)
+    MSE_young(i,:) = MSE_CG(RR_young(1+(i-1)*w:i*w), m_se, r, tau, scale, 1);
+    MDE_young(i,:) = MDE_MA(RR_young(1+(i-1)*w:i*w), m_de, B, scale);
+    
+end
+for i=1:floor(length(RR_elderly)/w)
+    MSE_elderly(i,:) = MSE_CG(RR_elderly(1+(i-1)*w:i*w), m_se, r, tau, scale, 1);
+    MDE_elderly(i,:) = MDE_MA(RR_elderly(1+(i-1)*w:i*w), m_de, B, scale);
 end
 
 % MSE results plot
 figure('position', [0, 0, 1150, 450]);
 x=1:scale; xlim([0 scale+1]);
 xlabel('Scale', 'fontsize', 20); ylabel('Value', 'fontsize', 20);
+title('MSE results')
 set(gca,'FontSize',20)
 hold on;
-errorbar(mean(MSE_chf), std(MSE_chf,[],2), '-kd', 'linewidth', 2, 'markersize', 10);
-errorbar(mean(MSE_elderly), std(MSE_elderly,[],2), '-bs', 'linewidth', 2, 'markersize', 10);
-errorbar(mean(MSE_CG_young), std(MSE_CG_young,[],2), '-ro', 'linewidth', 2, 'markersize', 10);
+errorbar(mean(MSE_chf,1), std(MSE_chf,[],1), '-kd', 'linewidth', 2, 'markersize', 10);
+errorbar(mean(MSE_elderly,1), std(MSE_elderly,[],1), '-bs', 'linewidth', 2, 'markersize', 10);
+errorbar(mean(MSE_young,1), std(MSE_young,[],1), '-ro', 'linewidth', 2, 'markersize', 10);
+legend('CHF patients', 'Elderly group', 'Young group')
 
 % MDE results plot
 figure('position', [0, 0, 1150, 450]);
 x=1:scale; xlim([0 scale+1]);
 xlabel('Scale', 'fontsize', 20); ylabel('Value', 'fontsize', 20);
+title('MDE results')
 set(gca,'FontSize',20)
 hold on;
-errorbar(mean(MDE_chf), std(MDE_chf,[],2), '-kd', 'linewidth', 2, 'markersize', 10);
-errorbar(mean(MDE_elderly), std(MDE_elderly,[],2), '-bs', 'linewidth', 2, 'markersize', 10);
-errorbar(mean(MDE_young), std(MDE_young,[],2), '-ro', 'linewidth', 2, 'markersize', 10);
+errorbar(mean(MDE_chf,1), std(MDE_chf,[],1), '-kd', 'linewidth', 2, 'markersize', 10);
+errorbar(mean(MDE_elderly,1), std(MDE_elderly,[],1), '-bs', 'linewidth', 2, 'markersize', 10);
+errorbar(mean(MDE_young,1), std(MDE_young,[],1), '-ro', 'linewidth', 2, 'markersize', 10);
+legend('CHF patients', 'Elderly group', 'Young group', 'Location', 'southeast')
 
 %% Performance evalutaion
 % Statistical analysis (p-value), classification accuracy, AUC value
 
-[p_mse(1,:), acc_mse(1,:), AUC_mse(1,:)] = performance_eval(MSE_chf, MSE_elderly, scale);
-[p_mse(2,:), acc_mse(2,:), AUC_mse(2,:)] = performance_eval(MSE_chf, MSE_young, scale);
-[p_mse(3,:), acc_mse(3,:), AUC_mse(3,:)] = performance_eval(MSE_young, MSE_elderly, scale);
+% [p_mse(1,:), acc_mse(1,:), AUC_mse(1,:)] = performance_eval(MSE_chf, MSE_elderly, scale);
+% [p_mse(2,:), acc_mse(2,:), AUC_mse(2,:)] = performance_eval(MSE_chf, MSE_young, scale);
+% [p_mse(3,:), acc_mse(3,:), AUC_mse(3,:)] = performance_eval(MSE_young, MSE_elderly, scale);
 
 [p_mde(1,:), acc_mde(1,:), AUC_mde(1,:)] = performance_eval(MDE_chf, MDE_elderly, scale);
 [p_mde(2,:), acc_mde(2,:), AUC_mde(2,:)] = performance_eval(MDE_chf, MDE_young, scale);
